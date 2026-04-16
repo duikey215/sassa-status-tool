4c389e87ae61ebbb6a346b43c8639ce1578007b6
duikey215 <duikey215@gmail.com>
2026-04-17 03:40:10 +0530

    index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,18 @@
 
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+@app.get("/api/check")
+def check_status(id_num: str, phone: str):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36',
+        'Referer': 'https://srd.sassa.gov.za/'
+    }
+    api_url = f"https://srd.sassa.gov.za/sc19/status/{id_num}/{phone}"
+    try:
+        r = requests.get(api_url, headers=headers, timeout=10)
+        return r.json()
+    except:
+        return {"error": "Server Busy. Try Again."}
